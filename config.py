2bfc78b2803995b23b685ea1c9db513345f7d709
James Droste <james@droste.im>
2016-04-03 19:06:31 -0400

    Removed SECRET_KEY

--- config.py
+++ config.py
@@ -3,8 +3,6 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
-SECRET_KEY = 'Wow, such secret'
-
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
